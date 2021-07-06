706d0b3eacdb58225c017e6729ac9562bd1e4919
unknown <ebene2009@gmail.com>
2021-07-06 23:36:03 +0100

    added usercontroller and route for displaying notifications

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DiscussionController;
 use App\Http\Controllers\RepliesController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -32,3 +33,5 @@ Route::resource('discussion', DiscussionController::class);
 Route::resource('discussion/{discussion}/replies', RepliesController::class);
 
 Route::post('discussion/{discussion}/replies/{reply}/mark-as-bast-reply', [DiscussionController::class, 'bestReply'])->name('discussion.bestReply');
+
+Route::get('user/notification', [UserController::class, 'notifications']);
